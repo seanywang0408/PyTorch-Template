3babb7c58c1f1d23d958c6d2529bfde02fd11753
Xiaoyang Huang <1043653815@qq.com>
2021-02-22 17:57:17 +0800

    Update torch_utils.py

--- mylib/torch_utils.py
+++ mylib/torch_utils.py
@@ -83,9 +83,10 @@ def set_seed(seed):
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
     else: 
         print('No seed is specified, therefore seed is collected from system clock.')
-
 def initialize(modules):
     for m in modules:
         if isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose3d):
